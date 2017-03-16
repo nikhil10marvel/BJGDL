3afb5c1bd7de1d19518b1005ea2f6e2d1e59aa06
Nikhil <nikhiljerry009@gmail.com>
2017-03-16 16:34:52 +0530

    Added Comments for Test Package Main Class

--- src/com/nikhil/test/Main.java
+++ src/com/nikhil/test/Main.java
@@ -16,8 +16,8 @@ public class Main {
 		Game game = new Game(800, 600, "Test Game", false);
 		game.getHandler().addObject(new Player(10, 10, "simple_cube"));
 		Entity cube = game.getHandler().findEntity("simple_cube");
-		cube.setDX(1);
-		cube.setDY(1);
+		cube.setDX(1.3663333f); //Diagonal movement is in the ration 600:800, 1: 1.333333, 1.3663333 is approx to that 
+		cube.setDY(1);	//The numbers are obtaibed through trial and error
 	}
 
 }
